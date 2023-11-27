8fd8d82d39f8464d1e86c6960444911b827546c7
William Zhang <81535097+willyum00@users.noreply.github.com>
2023-11-27 11:02:19 -0800

    grade.sh fix

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -12,5 +12,36 @@ echo 'Finished cloning'
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
+#grading-area
+#|
+#|______student-submission
+#|      |
+#       |_______Student's ListExamples.java
+#       |
+
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+set -e
+cd student-submission
+ls > /c/Users/will/Documents/GitHub/list-examples-grader/ls.txt
+
+#check that the correct files are submitted
+if [ -f "ListExamples.java" ]
+then
+    echo "correct file submitted" 
+    cp  /c/Users/will/Documents/GitHub/list-examples-grader/student-submission/ListExamples.java /c/Users/will/Documents/GitHub/list-examples-grader/grading-area   # make grading area absolute path
+    cp  /c/Users/will/Documents/GitHub/list-examples-grader/TestListExamples.java /c/Users/will/Documents/GitHub/list-examples-grader/grading-area
+else
+    echo "correct file not submitted"
+    exit 1
+fi
+
+set +e
+cd /c/Users/will/Documents/GitHub/list-examples-grader/
+cd grading-area
+javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" *.java
+java -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > output.txt
+echo $?
+
+
